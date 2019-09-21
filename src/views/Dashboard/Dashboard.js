dfb939d9c967b2a3dc0663ef9f22e2a3306dd424
Allen Wong <alnaym30@gmail.com>
2019-09-22 05:34:16 +0800

    Refine dashboard layout in mobile

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -15,39 +15,35 @@ import SearchInput from './SearchInput';
 function Dashboard({ history }) {
   return (
     <div>
-      <Container fluid>
-        <div className="animated fadeIn">
-          <Row>
-            <Col>
-              <SearchInput />
-            </Col>
-          </Row>
-        </div>
-      </Container>
+      <div className="animated fadeIn card-body">
+        <Row>
+          <Col>
+            <SearchInput />
+          </Col>
+        </Row>
+      </div>
       <NewsCarousel />
-      <Container fluid>
-        <div className="animated fadeIn">
-          <CardBody>
-            <Row>
-              {area.map(loc => (
-                <Col sm={4} lg={3} xs={6} className="px-2 my-1" key={loc.id}>
-                  <Card
-                    className={`m-0 bg-primary text-dark text-center pointer ${styles.button}`}
-                    onClick={() => { history.push(`/members?area=${loc.id}`)}}
-                    style={{ 'background-image': `url(${loc.img})`, position: 'relative' }}
-                  >
-                    <CardBody>
-                      <div className={styles.overlay} />
-                      <img src={loc.map} width="100px" alt={loc.label}/>
-                      <h5>{loc.label}</h5>
-                    </CardBody>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-          </CardBody>
-        </div>
-      </Container>
+      <div className="animated fadeIn">
+        <CardBody>
+          <Row>
+            {area.map(loc => (
+              <Col sm={4} lg={3} xs={6} className="px-2 my-1" key={loc.id}>
+                <Card
+                  className={`m-0 bg-primary text-dark text-center pointer ${styles.button}`}
+                  onClick={() => { history.push(`/members?area=${loc.id}`)}}
+                  style={{ 'background-image': `url(${loc.img})`, position: 'relative' }}
+                >
+                  <CardBody>
+                    <div className={styles.overlay} />
+                    <img src={loc.map} width="100px" alt={loc.label}/>
+                    <h5>{loc.label}</h5>
+                  </CardBody>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </CardBody>
+      </div>
     </div>
   );
 }
